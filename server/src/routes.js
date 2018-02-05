97119d9fcf0bbd2eec1a7d2635fa04cfdfde7cbf
evan miller <emille4@clemson.edu>
2018-02-05 15:55:01 -0500

    fixed client git issue

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,9 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
   app.post('/register',
+    AuthenticationControllerPolicy.register, // call policy as middleware controller before authenticating
     AuthenticationController.register)
 }
