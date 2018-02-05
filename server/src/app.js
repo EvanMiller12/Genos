a0b00f1d55e66bd8d76a2e10ed6777a97724e601
evan miller <emille4@clemson.edu>
2018-02-05 08:52:20 -0500

    setting up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser') // process jSON easily
-const cors = require('cors') // allows cross origin sharing
-const morgan = require('morgan') // print logs
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan') // log service
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 })
 
