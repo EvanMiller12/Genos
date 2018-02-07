09557d96361fe48795433bbbe75844e6b6af6ed3
evan miller <emille4@clemson.edu>
2018-02-07 14:34:58 -0500

    rendering songs from array

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,13 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
+        <div
+          v-for="song in songs"
+          :key="song.title">
+          {{song.title}}
+          {{song.artist}}
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
@@ -12,6 +19,17 @@
 import Panel from '@/components/Panel'
 
 export default {
+  data () {
+    return {
+      songs: [
+        {
+          title: 'Float On',
+          artist: 'Modest Mouse',
+          album: 'Good News for People Who Love Bad'
+        }
+      ]
+    }
+  },
   components: {
     Panel
   }
