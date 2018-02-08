41a77fa09c258cef054855ae5f47d9c4cfdb26ce
evan miller <emille4@clemson.edu>
2018-02-08 09:51:10 -0500

    styled songs component a little

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -20,8 +20,38 @@
           </v-btn>
         <!-- </router-link> -->
         <div
+          class="song"
           v-for="song in songs"
           :key="song.id">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+
+              <v-btn
+                dark
+                class="cyan"
+                @click="navigateTo({
+                  name: 'song',
+                  params: {
+                    songId: song.id
+                  }
+                })">
+                View
+              </v-btn>
+            </v-flex>
+
+            <v-flex xs-6>
+              <img :src="song.albumImageUrl" :alt="song.title" class="albume-image">
+            </v-flex>
+          </v-layout>
           {{song.title}}
           {{song.artist}}
           {{song.album}}
@@ -59,4 +89,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+
+  .song-title {
+    font-size: 30px;
+  }
+
+  .song-artist {
+    font-size: 24px;
+  }
+
+  .song-genre {
+    font-size: 18px;
+  }
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
