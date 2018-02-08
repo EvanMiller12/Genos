5d6f800becbfbd2785ba221b696e0a1b0f9b264b
evan miller <emille4@clemson.edu>
2018-02-08 14:41:26 -0500

    created layout for ViewSong and brought in data

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,4 +1,61 @@
 <template>
+  <div>
+    <v-layout>
+      <!-- left col -->
+      <v-flex xs6>
+        <panel title="Song Metadata">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+            </v-flex>
+
+            <v-flex xs6>
+              <img :src="song.albumImageUrl" :alt="song.title" class="album-image">
+              <br>
+              {{song.album}}
+            </v-flex>
+          </v-layout>
+        </panel>
+      </v-flex>
+      <!-- end left col -->
+
+      <!-- right col -->
+      <v-flex xs6>
+        <panel title="Youtube Video" class="ml-2">
+          <!-- youtube embed -->
+        </panel>
+      </v-flex>
+      <!-- end right col -->
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs6 class="ml-2">
+        <panel  title="Lyrics">
+          <textarea
+            readonly
+            v-model="song.lyrics"
+          ></textarea>
+        </panel>
+      </v-flex>
+
+      <v-flex xs6 class="ml-2">
+        <panel  title="Tabs">
+          <textarea
+            readonly
+            v-model="song.tab"
+          ></textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -14,10 +71,46 @@ export default {
   async mounted () {
     // router is synced to store in main.js, so updates songId by route from song navTo method
     const songId = this.$store.state.route.params.songId
-    this.song = await SongsService.showSong(songId)
+    this.song = (await SongsService.showSong(songId)).data
+  },
+  components: {
+    Panel
   }
 }
 </script>
 
 <style scoped>
+   .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+
+  .song-title {
+    font-size: 30px;
+  }
+
+  .song-artist {
+    font-size: 24px;
+  }
+
+  .song-genre {
+    font-size: 18px;
+  }
+
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
+
+  textarea {
+    font-family: monospace;
+    width: 100%;
+    height: 600px;
+    border: none;
+    border-style: none;
+    border-color: transparent;
+    overflow: auto; /* scrollbar */
+    padding: 40px;
+  }
 </style>
