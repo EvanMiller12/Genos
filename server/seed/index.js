037328d8820fe8b25316740a0316cf1112a13d6f
evan miller <emille4@clemson.edu>
2018-02-10 11:28:59 -0500

    fixed bug in seed/index.js

--- server/seed/index.js
+++ server/seed/index.js
@@ -12,20 +12,20 @@ const bookmarks = require('./bookmarks.json')
 
 sequelize.sync({force: true})
   .then(async function () {
-    Promise.all(
+    await Promise.all(
       // run through json array and return new user object for each one
       users.map(user => {
         User.create(user)
       })
     )
 
-    Promise.all(
+    await Promise.all(
       songs.map(song => {
         Songs.create(song)
       })
     )
     // needs to be after user and songs b/c we need them to be created first to associate
-    Promise.all(
+    await Promise.all(
       bookmarks.map(bookmark => {
         Bookmark.create(bookmark)
       })
