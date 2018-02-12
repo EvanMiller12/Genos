48a3f9f592628139362ef547be58e603eeeb4bcd
evan miller <emille4@clemson.edu>
2018-02-12 13:30:46 -0500

    created recently viewed songs component

---
+++ client/src/components/Songs/RecentlyViewedSongs.vue
@@ -0,0 +1,63 @@
+<template>
+  <panel title="Recently Viewed Songs">
+    <v-data-table
+      :headers="headers"
+      :pagination.sync="pagination"
+      :items="songs">
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-right">
+          {{props.item.title}}
+        </td>
+        <td class="text-xs-right">
+          {{props.item.artist}}
+        </td>
+      </template>
+    </v-data-table>
+  </panel>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import SongHistoryService from '@/services/SongHistoryService'
+
+export default {
+  data () {
+    return {
+      // headers- array of the text- data for column and value- key referenced from bookmark array
+      headers: [
+        {
+          text: 'Title',
+          value: 'title'
+        },
+        {
+          text: 'Artist',
+          value: 'artist'
+        }
+      ],
+      // sort bookmorks by date created
+      pagination: {
+        sortBy: 'createdAt',
+        descending: true
+      },
+      songs: []
+    }
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn',
+      'user'
+    ])
+  },
+  async mounted () {
+    // if user is logged in, make request to backend for all users bookmarks
+    if (this.isUserLoggedIn) {
+      this.songs = (await SongHistoryService.getSongs({
+        userId: this.user.id
+      })).data
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
