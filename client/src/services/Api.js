63d5ff8612f2c9cf08f18aef15aceab71cab3ce1
evan miller <emille4@clemson.edu>
2018-02-13 07:34:38 -0500

    added auth header in client api.js

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,12 @@
 import axios from 'axios' // connection service
+import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`
+    baseURL: `http://localhost:8081/`,
+    // set authorization header to token if one exist
+    headers: {
+      Authorization: `Bearer ${store.state.token}`
+    }
   })
 }
