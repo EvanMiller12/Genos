e50913635ada10080e4d18e4422ee84c481ad064
evan miller <emille4@clemson.edu>
2018-02-13 08:08:38 -0500

    order history by date on server side before finding uniques

--- server/src/controllers/HistoriesController.js
+++ server/src/controllers/HistoriesController.js
@@ -18,6 +18,9 @@ module.exports = {
           {
             model: Songs
           }
+        ],
+        order: [
+          ['createdAt', 'DESC']
         ]
       })
         .map(history => history.toJSON())
